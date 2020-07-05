2d32a75244315a81399488bfba0173f9d9a90ff4
blackmambabryant <vishal.shangari@gmail.com>
2020-07-04 21:14:42 -0700

    small fix on BuilderContainer

--- src/components/Builder/BuilderContainer.tsx
+++ src/components/Builder/BuilderContainer.tsx
@@ -1,15 +1,11 @@
 import React, { ReactNode } from "react";
 import styled from "styled-components";
 
-const BuilderRender = styled.div`
+const BuilderContainer = styled.div`
   height: 100vh;
   width: 100vw;
   ${(props) => props.theme.flex.row}
   background: ${(props) => props.theme.colors.builderBg};
 `;
-
-const BuilderContainer: React.FC = ({ children }) => {
-  return <BuilderRender>{children}</BuilderRender>;
-};
 
 export default BuilderContainer;
